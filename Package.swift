09bc2719dfc2dafa6fb634e89aa91d6c06a81654
Laura Sempere <laura.sempere@kape.com>
2023-11-20 17:54:42 +0100

    PIA-906: Update WireGuard to 1.0.16-27

--- Package.swift
+++ Package.swift
@@ -4,44 +4,81 @@
 import PackageDescription
 
 let package = Package(
-    name: "PIAWireguard",
+    name: "WireGuardKit",
     platforms: [
-      .iOS(.v12), .tvOS(.v17)
+        .iOS(.v12)
+//        .tvOS(.v17)
     ],
     products: [
-        .library(
-            name: "PIAWireguard", 
-            targets: [
-                "PIAWireguard",
-                "PIAWireguardC",
-                "PIAWireguardGo",
-            ]
-        ),
+        .library(name: "WireGuardKit", targets: ["WireGuardKit"])
     ],
-    dependencies: [
-        .package(url: "https://github.com/bitmark-inc/tweetnacl-swiftwrap.git", .exact("1.1.0")),
-        .package(url: "https://github.com/Alamofire/Alamofire.git", .exact("5.0.1")),
-
-    ],
+    dependencies: [],
     targets: [
         .target(
-            name: "PIAWireguard", 
-            dependencies: [
-                "PIAWireguardC",
-                "Alamofire",
-                .product(name: "TweetNacl", package: "tweetnacl-swiftwrap"),
-            ],
-            resources: [
-                .process("Resources")
-            ]
+            name: "WireGuardKit",
+            dependencies: ["WireGuardKitGo", "WireGuardKitC"]
         ),
         .target(
-            name: "PIAWireguardC", 
-            dependencies: [ ]
+            name: "WireGuardKitC",
+            dependencies: [],
+            publicHeadersPath: "."
         ),
-        .binaryTarget(
-            name: "PIAWireguardGo",
-            path: "PIAWireguardGo.xcframework"
-        ),
+        .target(
+            name: "WireGuardKitGo",
+            dependencies: [],
+            exclude: [
+                "goruntime-boottime-over-monotonic.diff",
+                "go.mod",
+                "go.sum",
+                "api-apple.go",
+                "Makefile"
+            ],
+            publicHeadersPath: ".",
+            linkerSettings: [.linkedLibrary("wg-go")]
+        )
     ]
 )
+
+
+//let package = Package(
+//    name: "PIAWireguard",
+//    platforms: [
+//      .iOS(.v12), .tvOS(.v17)
+//    ],
+//    products: [
+//        .library(
+//            name: "PIAWireguard", 
+//            targets: [
+//                "PIAWireguard",
+//                "PIAWireguardC",
+//                "PIAWireguardGo",
+//            ]
+//        ),
+//    ],
+//    dependencies: [
+//        .package(url: "https://github.com/bitmark-inc/tweetnacl-swiftwrap.git", .exact("1.1.0")),
+//        .package(url: "https://github.com/Alamofire/Alamofire.git", .exact("5.0.1")),
+//
+//    ],
+//    targets: [
+//        .target(
+//            name: "PIAWireguard", 
+//            dependencies: [
+//                "PIAWireguardC",
+//                "Alamofire",
+//                .product(name: "TweetNacl", package: "tweetnacl-swiftwrap"),
+//            ],
+//            resources: [
+//                .process("Resources")
+//            ]
+//        ),
+//        .target(
+//            name: "PIAWireguardC", 
+//            dependencies: [ ]
+//        ),
+//        .binaryTarget(
+//            name: "PIAWireguardGo",
+//            path: "PIAWireguardGo.xcframework"
+//        ),
+//    ]
+//)
