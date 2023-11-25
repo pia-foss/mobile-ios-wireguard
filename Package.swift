b9a2e0ff70d90019695f8357f0cd707120dcfd9e
Laura Sempere <laura.sempere@kape.com>
2023-11-26 00:31:19 +0100

    PIA-906: Downgrade swift-tools version to 5.5

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.9
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "PIAWireguard",
     platforms: [
-      .iOS(.v12), .tvOS(.v17)
+      .iOS(.v12)
     ],
     products: [
         .library(
